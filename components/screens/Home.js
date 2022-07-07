c781d5cfb55a518e336706176ad79026d0645cef
omairys <C0807503@mylambton.ca>
2022-07-07 03:02:24 -0400

    some functionalities for the shopping cart
    some functionalities for the shopping cart

--- components/screens/Home.js
+++ components/screens/Home.js
@@ -12,105 +12,215 @@ import Entypo from 'react-native-vector-icons/Entypo';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-const Home = () => {
+const Home = ({navigation}) => {
+  const [laptops, setLaptops] = useState([]);
+  const [macbooks, setAccessory] = useState([]);
+
+  //get called on screen loads
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getDataFromDB();
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  //get data from DB
+
+  const getDataFromDB = () => {
+    let laptopsList = [];
+    let macbooksList = [];
+    for (let index = 0; index < Items.length; index++) {
+      if (Items[index].category == 'laptops') {
+        laptopsList.push(Items[index]);
+      } else if (Items[index].category == 'macbooks') {
+        macbooksList.push(Items[index]);
+      }
+    }
+    setLaptops(laptopsList);
+    setAccessory(macbooksList);
+  };
+
+  //create an product reusable card
+  const ProductCard = ({data}) => {
     return (
-        <View
+      <TouchableOpacity
+        onPress={() => navigation.navigate('ProductInfo', {productID: data.id})}
         style={{
-          width: '100%',
-          height: '100%',
-          backgroundColor: COLOURS.white,
+          width: '48%',
+          marginVertical: 14,
         }}>
-        <StatusBar backgroundColor={COLOURS.white} barStyle="dark-content" />
-        <ScrollView showsVerticalScrollIndicator={false}>
-            <View
+        <View
+          style={{
+            width: '100%',
+            height: 100,
+            borderRadius: 10,
+            backgroundColor: COLOURS.backgroundLight,
+            position: 'relative',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: 8,
+          }}>
+          <Image
+            source={data.productImage}
             style={{
-                width: '100%',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                padding: 16,
-            }}>
-                <TouchableOpacity>
-                <Entypo
-                    name="shopping-bag"
-                    style={{
-                        fontSize: 18,
-                        color: COLOURS.backgroundMedium,
-                        padding: 12,
-                        borderRadius: 10,
-                        backgroundColor: COLOURS.backgroundLight,
-                    }}
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity>
-                <MaterialCommunityIcons
-                    name="cart"
-                    style={{
-                        fontSize: 18,
-                        color: COLOURS.backgroundMedium,
-                        padding: 12,
-                        borderRadius: 10,
-                        borderWidth: 1,
-                        borderColor: COLOURS.backgroundLight,
-                    }}
-                    />
-                </TouchableOpacity>
-            </View>
+              width: '80%',
+              height: '80%',
+              resizeMode: 'contain',
+            }}
+          />
+        </View>
+        <Text
+          style={{
+            fontSize: 12,
+            color: COLOURS.black,
+            fontWeight: '600',
+            marginBottom: 2,
+          }}>
+          {data.productName}
+        </Text>
+        { data.isAvailable ? (
             <View
-                style={{
-                    marginBottom: 10,
-                    padding: 16,
-                }}>
-                <Text
-                    style={{
-                    fontSize: 26,
-                    color: COLOURS.black,
-                    fontWeight: '500',
-                    letterSpacing: 1,
-                    marginBottom: 10,
-                    }}>
-                    Lambton Online Shop
-                </Text>
-                <Text
-                    style={{
-                    fontSize: 14,
-                    color: COLOURS.black,
-                    fontWeight: '400',
-                    letterSpacing: 1,
-                    lineHeight: 24,
-                    }}>
-                    Tech Shop on 265 Yorkland Blvd.
-                    {'\n'}This shop offers tech products for you
-                </Text>
-            </View>
-            <View
-                style={{
+              style={{
                 flexDirection: 'row',
                 alignItems: 'center',
-                justifyContent: 'space-between',
-                }}>
-            <View
+              }}>
+              <FontAwesome
+                name="circle"
                 style={{
+                  fontSize: 12,
+                  marginRight: 6,
+                  color: COLOURS.green,
+                }}
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: COLOURS.green,
+                }}>
+                Available
+              </Text>
+            </View>
+          ) : (
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <FontAwesome
+                name="circle"
+                style={{
+                  fontSize: 12,
+                  marginRight: 6,
+                  color: COLOURS.red,
+                }}
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: COLOURS.red,
+                }}>
+                Unavailable
+              </Text>
+            </View>
+          )
+        }
+        <Text>&#36; {data.productPrice}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View
+      style={{
+        width: '100%',
+        height: '100%',
+        backgroundColor: COLOURS.white,
+      }}>
+      <StatusBar backgroundColor={COLOURS.white} barStyle="dark-content" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            width: '100%',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            padding: 16,
+          }}>
+          <TouchableOpacity>
+            <Entypo
+              name="shopping-bag"
+              style={{
+                fontSize: 18,
+                color: COLOURS.backgroundMedium,
+                padding: 12,
+                borderRadius: 10,
+                backgroundColor: COLOURS.white,
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('MyCart')}>
+            <MaterialCommunityIcons
+              name="cart"
+              style={{
+                fontSize: 18,
+                color: COLOURS.backgroundMedium,
+                padding: 12,
+                borderRadius: 10,
+                backgroundColor: COLOURS.backgroundLight,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            marginBottom: 10,
+            padding: 16,
+          }}>
+          <Text
+            style={{
+              fontSize: 26,
+              color: COLOURS.black,
+              fontWeight: '500',
+              letterSpacing: 1,
+              marginBottom: 10,
+            }}>
+            Explore Categories
+          </Text>
+
+        </View>
+        <View
+          style={{
+            padding: 16,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <View
+              style={{
                 flexDirection: 'row',
                 alignItems: 'center',
               }}>
               <Text
                 style={{
-                    fontSize: 18,
-                    color: COLOURS.black,
-                    fontWeight: '500',
-                    letterSpacing: 1,
+                  fontSize: 18,
+                  color: COLOURS.black,
+                  fontWeight: '500',
+                  letterSpacing: 1,
                 }}>
-                Products
+                Laptops
               </Text>
               <Text
                 style={{
-                    fontSize: 14,
-                    color: COLOURS.black,
-                    fontWeight: '400',
-                    opacity: 0.5,
-                    marginLeft: 10,
+                  fontSize: 14,
+                  color: COLOURS.black,
+                  fontWeight: '400',
+                  opacity: 0.5,
+                  marginLeft: 10,
                 }}>
-                41
+                {laptops.length}
               </Text>
             </View>
             <Text
@@ -122,9 +232,73 @@ const Home = () => {
               SeeAll
             </Text>
           </View>
-        </ScrollView>
-       </View>
-    );
-  }
+          <View
+            style={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent: 'space-around',
+            }}>
+            {laptops.map(data => { return <ProductCard data={data} key={data.id} />; })}
+          </View>
+        </View>
 
-export default Home
+        <View
+          style={{
+            padding: 16,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  color: COLOURS.black,
+                  fontWeight: '500',
+                  letterSpacing: 1,
+                }}>
+                Mackbooks
+              </Text>
+              <Text
+                style={{
+                  fontSize: 14,
+                  color: COLOURS.black,
+                  fontWeight: '400',
+                  opacity: 0.5,
+                  marginLeft: 10,
+                }}>
+                {macbooks.length}
+              </Text>
+            </View>
+            <Text
+              style={{
+                fontSize: 14,
+                color: COLOURS.blue,
+                fontWeight: '400',
+              }}>
+              SeeAll
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent: 'space-around',
+            }}>
+            {macbooks.map(data => {return <ProductCard data={data} key={data.id} />;
+            })}
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default Home;
