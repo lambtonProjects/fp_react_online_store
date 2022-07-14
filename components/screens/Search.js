16ceda18868c3b606cb3fd2ae62dc2a49839ce4f
Sheamus <c0807605@mylambton.ca>
2022-07-13 23:59:59 -0400

    Search page development.

--- components/screens/Search.js
+++ components/screens/Search.js
@@ -1,23 +1,39 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import ProductList from '../views/ProductList';
 import { Items } from '../database/Database';
 
 const Search = ({navigation}) => {
 
+  const [keyword, setKeyword] = useState('');
+  const[items, setItems] = useState([]);
+
+  const onTextChanged = (text) => {
+    setKeyword(text);
+    setItems(Items.filter((item) => {
+      return item.productName.includes(text);
+    }));
+    
+  }
+
+
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
       // getDataFromDB();
+      setItems(Items)
+      console.log("keyword......")
+
     });
 
+    
     return unsubscribe;
   }, [navigation]);
 
   
     return (
       <View>
-        <TextInput style={styles.searchInput} placeholder='Product Name' />
-        <ProductList items={Items}  navigateToDetail={navigation}/>
+        <TextInput style={styles.searchInput} placeholder='Product Name' value={keyword} onChangeText={(item) => { onTextChanged(item)  }} />
+        <ProductList items={items}  navigateToDetail={navigation}/>
        </View>
     );
   }
